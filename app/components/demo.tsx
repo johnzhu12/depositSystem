9d449744de85dce8bb4ff64683fc77756414ae7a
johnzhu12 <1042532367@qq.com>
2018-09-09 14:57:26 +0800

    update

--- app/components/demo.tsx
+++ app/components/demo.tsx
@@ -1,79 +1,56 @@
 //Greeter,js
 import * as React from 'react';
-import { DatePicker, Button } from 'antd'
+import { Col, Row } from 'antd'
 import * as moment from 'moment'
 
-interface DemoStates {
-    startDate: string;
-    endDate: string;
+interface States {
+
 }
-interface DemoProps {
+interface Props {
 
 }
 
-class Demo extends React.Component<DemoProps, DemoStates> {
+class Demo extends React.Component<Props, States> {
     cacheStartDate: string;
     cacheEndDate: string;
-    constructor(props: DemoProps) {
+    constructor(props: Props) {
         super(props)
-        this.state = {
-            startDate: '',
-            endDate: ''
-        }
-        this.cacheStartDate = '';
-        this.cacheEndDate = '';
+
     }
 
-    choseStartDate(date, dateString) {
-        if (dateString) {
-            this.setState({ startDate: dateString })
-            // this.cacheStartDate 
-        }
-    }
-    choseEndDate(date, dateString) {
-        if (dateString) {
-            this.setState({ endDate: dateString })
-            // this.cacheStartDate 
-        }
-    }
+
     submit() {
 
     }
-    clear() {
-        this.setState({
-            startDate: '', endDate: ''
-        })
-    }
+
     render() {
-        const startDateProps = {
-            placeholder: "开始日期",
-            onChange: this.choseStartDate.bind(this)
-        }
-        const endDateProps = {
-            placeholder: "结束日期",
-            onChange: this.choseEndDate.bind(this)
-        }
-        if (this.state.startDate) {
-            startDateProps['value'] = moment(this.state.startDate, 'YYYY-MM-DD')
-        } else if (this.state.startDate == '') {
-            startDateProps['value'] = ''
-        }
-        if (this.state.endDate) {
-            endDateProps['value'] = moment(this.state.endDate, 'YYYY-MM-DD')
-        } else if (this.state.endDate == '') {
-            endDateProps['value'] = ''
-        }
+
         return (
             <div>
-                <DatePicker {...startDateProps} />~
-                <DatePicker {...endDateProps} />
+                <Row style={{ width: '100%', height: '80px' }}>
+                    <Col span={4} style={{ background: 'red', height: '80px' }}>
+                    </Col>
+                    <Col span={2} style={{ background: 'blue', height: '80px' }}>
+                    </Col>
+                    <Col span={14} style={{ background: 'black', height: '80px' }}>
+                    </Col>
+                    <Col span={4} style={{ background: 'red', height: '80px' }}>
+                    </Col>
+                </Row>
+                <div style={{ background: 'white', height: '2px' }}>
 
-                <div style={{ marginTop: '20px' }}>
-                    <Button type="primary" onClick={this.submit.bind(this)}>点我</Button>
-                    <Button type="primary" onClick={this.clear.bind(this)}>清空</Button>
                 </div>
+                <Row style={{ width: '100%', height: '80px' }}>
+                    <Col span={4} style={{ background: 'red', height: '80px' }}>
+                    </Col>
 
-            </div >
+                    <Col span={16} style={{ background: 'black', height: '80px' }}>
+                    </Col>
+                    <Col span={4} style={{ background: 'red', height: '80px' }}>
+
+                    </Col>
+                </Row>
+            </div>
 
         );
     }
